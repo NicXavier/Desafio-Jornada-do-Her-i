efbe19c752e245d76f13b475cbaed300e2e462d8
NicXavier <notedenic@gmail.com>
2024-02-09 16:08:32 -0300

    Add explanation of the first challenge

--- classificacaoNível.js
+++ classificacaoNível.js
@@ -1,3 +1,13 @@
+//#Desafio Classificador de nível de Herói
+
+//**O que deve ser utilizado**
+//- Variáveis
+//- Operadores 
+//- Laços de repetição
+//- Estrutura de decisões
+
+//##Objetivo
+//Crie uma variável para armazenar o nome e a quantidade de experiência (XP) de um herói, depois utilize uma estrutura de decisão para apresentar alguma das mensagens abaixo:
 //Nome de Herói e XP
 var heroi = {
     nome: "Super Herói",
@@ -6,6 +16,15 @@ var heroi = {
 
 //Variável para armazenar o nível do Herói
 let nivel;
+
+//Se XP for menor do que 1.000 = Ferro
+//Se XP for entre 1.001 e 2.000 = Bronze
+//Se XP for entre 2.001 e 5.000 = Prata
+//Se XP for entre 5.001 e 7.000 = Ouro
+//Se XP for entre 7.001 e 8.000 = Platina
+//Se XP for entre 8.001 e 9.000 = Ascendente
+//Se XP for entre 9.001 e 10.000 = Imortal 
+//Se XP for maior ou igual 10.000 = Radiante
 
 //Verificando quantidade de XP e atribuindo nível
 if (heroi.xp <= 1000){
@@ -26,6 +45,9 @@ if (heroi.xp <= 1000){
     nivel = "Radiante";
 }
 
+//##Saída
 
-//Mensagem final
+//Ao final deve se exibir uma mensagem:
+//"O Herói de nome **{nome}** está no nível de **{nível}**"
+
 console.log("O Herói de nome " +heroi.nome+ " está no nível de " +nivel+ ".");
